3020c73b31a43970aa161e57d2c03b05857e2e19
gabrieladelfino <gabriela.delfino@outlook.com.br>
2019-05-02 10:56:07 -0300

    Adiciona informações de login

--- src/Pages/Profile/Profile.hoc.js
+++ src/Pages/Profile/Profile.hoc.js
@@ -1,4 +1,34 @@
-import { compose, withState, withHandlers } from 'recompose'
+import { compose, withState, withHandlers, lifecycle } from 'recompose'
+import { withFormik } from 'formik'
+import { getProfile } from '../../services'
+
+const request = new Request(getProfile, {
+  method: 'GET',
+  headers: new Headers(),
+  mode: 'cors',
+  cache: 'default'
+});
+
+const getProfileInformation = () => {
+  fetch(request).then(response => {
+    return response.json().then(json => {
+      console.log(json)
+      return json
+    })
+  })
+}
+
+export const showInformation = withFormik({
+  mapPropsToValues: data => ({
+    register: {
+      name: data.name,
+      username: data.username,
+      email: data.email,
+      confirmEmail: data.confirmEmail,
+      password: data.password
+    }
+  })
+})
 
 const enhance = compose(
   withState('value', 'setValue', ''),
@@ -7,15 +37,22 @@ const enhance = compose(
   withState('isDisable', 'setIsDisabled', true),
   withHandlers({
     showObjetives: () => () => {
-      console.log("AAAA");
-      document.getElementById("conquistas").style.display = "none";
-      document.getElementById("objetivos").style.display = "inline";
+      document.getElementById('conquistas').style.display = 'none';
+      document.getElementById('objetivos').style.display = 'inline';
     },
     showAchievements: () => () => {
-      console.log("BBBB");
-      document.getElementById("objetivos").style.display = "none";
-      document.getElementById("conquistas").style.display = "inline";
+      document.getElementById('objetivos').style.display = 'none';
+      document.getElementById('conquistas').style.display = 'inline';
     },
+  }),
+  lifecycle({
+    componentDidMount() {
+      const { setIsDisabled } = this.props
+      setIsDisabled(true)
+
+      const data = getProfileInformation()
+      showInformation(data)
+    }
   })
 )
 
--- src/services/services.js
+++ src/services/services.js
@@ -2,3 +2,4 @@ const BASE = 'localhost:8080'
 
 export const register = `${BASE}/include`
 export const login = `${BASE}/login`
+export const getProfile = `${BASE}/profile/`
