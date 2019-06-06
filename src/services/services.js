d5bfbf8da6ba83e4d326a571b1096a1a58dc73fa
Gabriela Delfino <gabriela.delfino@outlook.com.br>
2019-06-05 21:10:12 -0300

    Altera URL

--- src/services/services.js
+++ src/services/services.js
@@ -2,4 +2,4 @@ const BASE = 'http://serviceyouthquake.azurewebsites.net'
 
 export const register = `${BASE}/include`
 export const login = `${BASE}/login`
-export const profile = `${BASE}/profile/`
+export const profile = `${BASE}/profile`
